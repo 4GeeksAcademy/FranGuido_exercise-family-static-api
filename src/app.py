6c76938fa6ab2e3e4eef444bf9a596ad7a989d0e
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-08-17 01:00:22 +0000

    update

--- src/app.py
+++ src/app.py
@@ -31,10 +31,7 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "Jackson Family": members
-    }
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
 # GETTING SINGLE MEMBER BY ID
 @app.route('/members/<int:id>', methods=['GET'])
